0c33478899952fbca7e7ada9462e2dbe762b340a
Sarah0215 <sakurako.ino@gmail.com>
2024-04-29 20:00:16 -0700

    final commit schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -5,29 +5,9 @@
 -- ETL Mini Project
 -- M. Dai, S. Kikuchi, J. Bein
 
-CREATE TABLE Subcategory (
-    subcategory_ID VARCHAR(10) PRIMARY KEY,
-    subcategory VARCHAR(20)   NOT NULL,
-    FOREIGN KEY (subcategory_ID) REFERENCES Campaign(subcategory_id)
-);
-
-CREATE TABLE Category (
-    category_ID VARCHAR(5) PRIMARY KEY,
-    category VARCHAR(20)   NOT NULL,
-    FOREIGN KEY (category_ID) REFERENCES Campaign(category_id)
-);
-
-CREATE TABLE Contacts (
-    contact_ID INT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    FOREIGN KEY (contact_ID) REFERENCES Campaign(contact_id)
-);
-
 CREATE TABLE Campaign (
-    cf_id INT PRIMARY KEY
-    contact_id INT   NOT NULL,
+    cf_id INT PRIMARY KEY,
+    contact_id INT NOT NULL,
     company_name VARCHAR(200)   NOT NULL,
     description VARCHAR(200)   NOT NULL,
     goal FLOAT   NOT NULL,
@@ -40,5 +20,27 @@ CREATE TABLE Campaign (
     end_date DATE   NOT NULL,
     category_id VARCHAR(5)   NOT NULL,
     subcategory_id VARCHAR(10)   NOT NULL,
+	FOREIGN KEY (category_id) REFERENCES Category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES Subcategory(subcategory_id),
+    FOREIGN KEY (contact_id) REFERENCES Contacts(contact_id)
 );
+
+CREATE TABLE Subcategory (
+    subcategory_id VARCHAR(10) PRIMARY KEY,
+    subcategory VARCHAR(20)   NOT NULL
+);
+
+CREATE TABLE Category (
+    category_id VARCHAR(5) PRIMARY KEY,
+    category VARCHAR(20)   NOT NULL
+);
+
+CREATE TABLE Contacts (
+    contact_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
+
 
